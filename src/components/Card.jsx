1874012ff07af2b9020a3f9b5b4e700a5138c0e8
Carlos Zalpa <nedhel.90@gmail.com>
2023-01-05 16:13:20 -0600

    Card function finished

---
+++ src/components/Card.jsx
@@ -0,0 +1,11 @@
+import '../styles/Card.css'
+export function Card(){
+    return(
+        <div className="cardContainer">
+            <div className='card'>
+                <div className="side front">Front</div>
+                <div className="side back">Back</div>
+            </div>                     
+        </div>
+    )
+}
