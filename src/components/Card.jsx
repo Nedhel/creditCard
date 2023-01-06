c024129337b059cc3b4ae264f5db44d1c0593b7a
Carlos Zalpa <nedhel.90@gmail.com>
2023-01-06 11:22:15 -0600

    card style finished

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,31 @@
 import '../styles/Card.css'
+import chip from '../assets/chip.png'
 export function Card(){
     return(
         <div className="cardContainer">
             <div className='card'>
-                <div className="side front">Front</div>
-                <div className="side back">Back</div>
+                <div className="side front">
+                    <div className='title'>
+                        <h2 className='bankName'>Tu Banco</h2>
+                        <p className='category'>Credito Oro</p>
+                    </div>
+                    <img src={chip} alt="chip de tarjeta" className='chip' />
+                    <div className='cardNumber'>
+                        <p>1 1 1 1</p>
+                        <p>2 2 2 2</p>
+                        <p>3 3 3 3</p>
+                        <p>4 4 4 4</p>
+                    </div>
+                    <h1 className='entity'>VISA</h1>
+                </div>
+                <div className="side back">
+                    <div className='magneticLine'></div>
+                    <div className='signContainer'>
+                        <div className='signSpace'></div>
+                        <div className='cvv'></div>
+                    </div>
+                    <p className='info'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vel porttitor diam. Etiam accumsan malesuada libero, egestas accumsan arcu vestibulum ut.</p>
+                </div>
             </div>                     
         </div>
     )
